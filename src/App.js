9a5db98558a80abe56595b045e5dac259ee75598
Ricardo Castro <ricardocastro.dev@gmail.com>
2022-03-04 22:23:33 +0100

    Fix Fetch and key props

--- src/App.js
+++ src/App.js
@@ -3,18 +3,19 @@ import MovieCard from './MovieCard'
 import SearchIcon from './search.svg'
 import './App.css'
 
-const API_URL = "http://www.omdbapi.com?apikey=10e94bcd"
+const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=10e94bcd"
+// http://www.omdbapi.com/?i=tt3896198&apikey=10e94bcd
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    searchMovies("Barnie");
+    searchMovies("Flash");
   }, []);
 
   const searchMovies = async (title) => {
-    const response = await fetch(`http://www.omdbapi.com?apikey=10e94bcd&s=${title}`);
+    const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
 
     setMovies(data.Search);
@@ -39,13 +40,13 @@ const App = () => {
 
       {movies?.length > 0 ? (
         <div className="container">
-          {movies.map((movie) => (
-            <MovieCard movie={movie} />
+          {movies.map((movie, i) => (
+            <MovieCard movie={movie} key={i} />
           ))}
         </div>
       ) : (
         <div className="empty">
-          <h2>No encontré tu asquerosa pelicula {":("}</h2>
+          <h2>No encontré tu pelicula amor {":("}</h2>
         </div>
       )}
     </div>
